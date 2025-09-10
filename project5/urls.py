eab8aa5aaa482263a0601355c2961a0eef3bb6ef
Nomisodan <124398186+Nomisodan@users.noreply.github.com>
2025-09-09 21:29:34 -0600

    updating files

--- project5/urls.py
+++ project5/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Capstone.urls')), 
+    path('', include('Capstone.urls')),  # Make sure Capstone/urls.py exists
 ]
